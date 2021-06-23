777d5c84d7918655e29a3a625109aeba420df6f8
henrfarias <henrique.biasibetti@gmail.com>
2021-06-23 12:08:25 -0300

    :construction: Create a first stack navigator with Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import Home from './Views/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Opened!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Views/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, Button, StyleSheet } from 'react-native';
+import ScreenContainer from '../Components/ScreenContainer';
+import { StatusBar } from 'expo-status-bar';
+
+const Home = () => {
+  return ( 
+    <ScreenContainer>
+      <Text>Home Page</Text>
+    </ScreenContainer>
+  );
+}
+
+export default Home;
